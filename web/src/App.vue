f67b71e764a6ce5d97840409af7b7f410d2b5cbd
Sidney Kochman <sidney@kochman.org>
2017-12-14 18:01:20 -0500

    Add web interface start

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <nav class="navbar is-danger" role="navigation">
+      <div class="navbar-brand">
+        <h1 class="navbar-item title">Poly uploader</h1>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '../node_modules/bulma/css/bulma.css';
+</style>
---
+++ web/src/components/PostEditor.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="section">
+    <h1 class="title">Post editor</h1>
+    <h2 class="subtitle">Turn an InDesign snippet into a WordPress post.</h2>
+    <div class="field">
+      <label class="label">Kicker</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="A kicker">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Headline</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="A headline">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Subdeck</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="A subdeck">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Author name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="An author name">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Author title</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="An author title">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Body text</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Some body text"></textarea>
+      </div>
+    </div>
+    <a class="button is-primary">Create post</a>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
